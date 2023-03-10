5459e7e626d12c679c396d5a0353004c0808f39d
RudezY <rudyyamashiro@yahoo.com>
2023-03-09 20:04:27 -0800

    fixed server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db= require('./config/connection');
 const routes = require('./routes');
-
+// changes
 const app = express();
 const PORT = process.env.PORT || 3001;
 
