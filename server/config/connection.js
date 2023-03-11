b50d29694d75bcdb38f35df4226c7acf473a9019
RudezY <rudyyamashiro@yahoo.com>
2023-03-11 12:22:33 -0800

    most fixes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+console.log(process.env.MONGODB_URI + ' this is a test for uri key ')
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/heart2heart', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- server/server.js
+++ server/server.js
@@ -1,34 +1,24 @@
-
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
-// Run npm install mongodb and require mongodb and MongoClient class
-const mongodb = require('mongodb').MongoClient;
+const db = require('./config/connection');
+const routes = require('./routes');
 
-
+const PORT = process.env.PORT || 3001;
 const app = express();
-const port = 3001;
-
-// Connection string to local instance of MongoDB including database name
-const connectionStringURI = `mongodb://127.0.0.1:27017/heart2heart`;
-
-// Declare a variable to hold the connection
-let db;
 
 
-// Creates a connection to a MongoDB instance and returns the reference to the database
-mongodb.connect(
-  // Defines connection between app and MongoDB instance
-  connectionStringURI,
-  // Sets connection string parser and Server Discover and Monitoring engine to true and avoids warning
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    // Use client.db() constructor to add new db instance
-    db = client.db();
-    app.listen(port, () => {
-      console.log(`Example app listening at http://localhost:${port}`);
-    });
-  }
-);
-
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+// app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server is running here ${PORT}`);
+  });
+})
+
+
+
 
 
