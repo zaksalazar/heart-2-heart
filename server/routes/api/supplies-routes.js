251b21a81f6ce427e85fab5959b774243b3aa70e
Dillon Kim <dillonkim@Dillons-Air.attlocal.net>
2023-03-16 19:24:32 -0700

    add user/supply routes, modify user/suppply models

--- server/routes/api/supplies-routes.js
+++ server/routes/api/supplies-routes.js
@@ -1,54 +1,135 @@
-const router = require('express').Router();
-const {Supply} = require("../../models")
+const router = require("express").Router();
+const { Supply } = require("../../models");
 
-// Route that gets all supplies
-router.get('/', async (req, res) => {
-    try {
-        const supplies = await Supply.find({})
-        res.status(200).json(supplies)
-    } catch (err) {
-        console.log(err)
-        res.send(err)
-    }
-})
+// dependencies needed for S3 storage of images
+const multer = require("multer");
+const Aws = require("aws-sdk");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// Route that gets single supply
-router.get('/:supplyId', async (req, res) => {
-    try {
-        const singleSupply = await Supply.findById(req.params.supplyId)
-        res.status(200).json(singleSupply)
-    } catch (err) {
-        console.log(err)
-        res.send(err)
+// creating the storage variable to upload the file and providing the destination folder,
+// if nothing is provided in the callback it will get uploaded in main directory
+const storage = multer.memoryStorage({
+  destination: function (req, file, cb) {
+    cb(null, "");
+  },
+});
+
+// below variable is define to check the type of file which is uploaded
+const filefilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/jpg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// defining the upload variable for the configuration of photo being uploaded
+const upload = multer({ storage: storage, fileFilter: filefilter });
+
+// Now creating the S3 instance which will be used in uploading photo to S3 bucket
+const s3 = new Aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+});
+
+// handle post request and upload photo (upload photo using key defined below in upload.single id: supplyImage)
+router.post("/upload", upload.single("supplyImage"), async (req, res) => {
+  console.log(req.file); // check data in console that is being uploaded
+  let s3ImgLocation;
+  try {
+    if (req.file) {
+      const params = {
+        Bucket: process.env.AWS_BUCKET_NAME, // bucket name
+        Key: req.file.originalname, // name of image
+        Body: req.file.buffer, // body which will contain the image in buffer format
+        ACL: "public-read", // defining the permissions to get public link
+        ContentType: "image/jpeg", // necessary to define the image content-type to view photo in browswer with link
+      };
+
+      // uploading photo using s3 instance and saving link in database
+      const data = await s3.upload(params).promise();
+
+      // if not then below code will be executed
+
+      console.log(data); // will give information about object in which photo is stored
+      s3ImgLocation = data.Location;
     }
-})
+    // save info in database
+    const supply = await Supply.create({
+      recipientId: req.body.recipientId,
+      title: req.body.title,
+      description: req.body.description,
+      tags: req.body.tags,
+      county: req.body.county,
+      supplyImage: s3ImgLocation
+    });
+    res.status(200).json(supply);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
 
 // Route that creates a new supply
-router.post('/', async (req, res) => {
-    try {
-        const supply = await Supply.create({
-            recipientId: req.body.recipientId,
-            fulfilled: req.body.fulfilled,
-            items: req.body.items,
-            description: req.body.description
-        })
-        res.status(200).json(supply)
-    } catch (err) {
-        console.log(err)
-        res.send(err)
-    }
-})
+// POST http://localhost:3001/api/supplies/
+// router.post("/", upload.single("supplyImage"), async (req, res) => {
+//   console.log(req.file);
+//   try {
+//     const supply = await Supply.create({
+//       recipientId: req.body.recipientId,
+//       title: req.body.title,
+//       description: req.body.description,
+//       tags: req.body.tags,
+//       county: req.body.county,
+//     });
+
+//     res.status(200).json(req.body);
+//   } catch (err) {
+//     console.log(err);
+//     res.send(err);
+//   }
+// });
+
+// Route that gets all supplies
+// GET http://localhost:3001/api/supplies/
+router.get("/", async (req, res) => {
+  try {
+    const supplies = await Supply.find({});
+    res.status(200).json(supplies);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+// Route that gets single supply
+// GET http://localhost:3001/api/supplies/:supplyId
+router.get("/:supplyId", async (req, res) => {
+  try {
+    const singleSupply = await Supply.findById(req.params.supplyId);
+    res.status(200).json(singleSupply);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
 
 // Route that updates single supply
-router.put('/:supplyId', async (req, res) => {
-    try {
-        const options = { new: true }
-        const updatedSupply = await Supply.findByIdAndUpdate(req.params.supplyId, req.body, options)
-        res.status(200).json(updatedSupply)
-    } catch (err) {
-        console.log(err)
-        res.send(err)
-    }
-})
+// PUT/POST http://localhost:3001/api/supplies/:supplyId
+router.put("/:supplyId", async (req, res) => {
+  try {
+    const options = { new: true };
+    const updatedSupply = await Supply.findByIdAndUpdate(
+      req.params.supplyId,
+      req.body,
+      options
+    );
+    res.status(200).json(updatedSupply);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
 
 module.exports = router;
--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const {User} = require("../../models")
+const {signToken} = require("../../utils/auth")
 
 // Route that logs in
+// POST http://localhost:3001/api/users/login
 router.post('/login', async ( {body}, res ) => {
     const user = await User.findOne({  username: body.username  });
     if (!user) {
@@ -13,13 +15,33 @@ router.post('/login', async ( {body}, res ) => {
     if (!correctPw) {
       return res.status(400).json({ message: 'Wrong password!' });
     }
-    res.json(user)
-    // const token = signToken(user);
-    // res.json({ token, user });
+    // res.json(user)
+    const token = signToken(user);
+    res.json({ token, user });
 })
 
+// Route that creates new user
+// POST http://localhost:3001/api/users/create-user
+router.post('/create-user', async (req,res) => {
+    try {
+        const user = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        password: req.body.password
+        })
+        // res.status(200).json(user)
+        const token = signToken(user);
+        res.json({ token, user });
+    } catch (err) {
+        console.log(err)
+        res.send(err)
+    }
+})
 
 // Route that gets all the users
+// GET http://localhost:3001/api/users/
 router.get('/', async (req, res) => {
     try {
         const users = await User.find({})
@@ -31,6 +53,7 @@ router.get('/', async (req, res) => {
 })
 
 // Route that gets single user based on user id
+// GET http://localhost:3001/api/users/:userId
 router.get('/:userId', async (req, res) => {
     try {
         const user = await User.findById(req.params.userId)
@@ -41,24 +64,9 @@ router.get('/:userId', async (req, res) => {
     }
 })
 
-// Route that creates new user
-router.post('/', async (req,res) => {
-    try {
-        const user = await User.create({
-        username: req.body.username,
-        email: req.body.email,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        password: req.body.password
-        })
-        res.status(200).json(user)
-    } catch (err) {
-        console.log(err)
-        res.send(err)
-    }
-})
 
 // Route that updates single user based on user id
+// PUT/POST http://localhost:3001/api/users/:userId
 router.put('/:userId', async (req, res) => {
     try {
         const options = {new: true}
