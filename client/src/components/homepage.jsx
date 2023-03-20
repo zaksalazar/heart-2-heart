dde85863e41b988b94678e580580f3309da286ac
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2023-03-20 07:47:30 -0700

    added zaks new logo

--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import background from "/public/assets/images/familyPlaceholder.jpeg";
 import Explainer from "./Explainer";
-import Team from "./Team";
 
 function Homepage() {
   const [show, setShow] = useState(false);
@@ -98,7 +97,14 @@ function Homepage() {
           }}
         >
           <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
-            <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
+            <div className="w-4/5 sm:w-2/3 lg:flex justify-center items-center flex-col">
+              <div className="flex flex-col justify-center items-center">
+                <img
+                  className="max-w-[300px]  md:w-auto "
+                  src="/assets/images/logo2.png"
+                  alt="photo"
+                />
+              </div>
               <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-black leading-7 md:leading-10">
                 People Helping People
               </h1>
@@ -110,11 +116,11 @@ function Homepage() {
                 people{" "}
               </p>
             </div>
-            <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center mt-4">
               <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-700 bg-red-700 transition duration-150 ease-in-out hover:bg-red-600 lg:text-xl lg:font-bold  rounded text-white px-4 sm:px-10 border border-red-700 py-2 sm:py-4 text-sm">
                 Need Help?
               </button>
-              <button className="ml-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-700 bg-transparent transition duration-150 ease-in-out hover:border-red-600 lg:text-xl lg:font-bold  hover:text-red-600 rounded border border-red-700 text-red-700 px-4 sm:px-10 py-2 sm:py-4 text-sm">
+              <button className="ml-4 focus:outline-none bg-white focus:ring-2 focus:ring-offset-2 focus:ring-red-700 bg-transparent transition duration-150 ease-in-out hover:border-red-600 lg:text-xl lg:font-bold  hover:text-red-600 rounded border border-red-700 text-red-700 px-4 sm:px-10 py-2 sm:py-4 text-sm">
                 Help a Family
               </button>
             </div>
