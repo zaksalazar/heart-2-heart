7ec73886c9ab5aab9c39e17805261ecf8fafb90d
zaksalazar <zsalazar.tv@gmail.com>
2023-03-20 22:22:37 -0700

    ok

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
